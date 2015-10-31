f059e631ded99084243aea0b192e11fd036e2437
Matthieu-Lescieux <MatthieuLescieux@hotmail.fr>
2015-10-31 12:06:36 +0100

    Handle several parent types if useInterface configuration is set to true

--- src/TypesGeneratorConfiguration.php
+++ src/TypesGeneratorConfiguration.php
@@ -31,107 +31,107 @@ class TypesGeneratorConfiguration implements ConfigurationInterface
         $rootNode = $treeBuilder->root('config');
         $rootNode
             ->children()
-                ->arrayNode('rdfa')
-                    ->info('RDFa files to use')
-                    ->defaultValue([
-                        self::SCHEMA_ORG_RDFA_URL,
-                    ])
-                    ->prototype('scalar')->end()
-                ->end()
-                ->arrayNode('relations')
-                    ->info('OWL relation files to use')
-                    ->defaultValue([
-                        self::GOOD_RELATIONS_OWL_URL,
-                    ])
-                    ->prototype('scalar')->end()
-                ->end()
-                ->booleanNode('debug')->defaultFalse()->info('Debug mode')->end()
-                ->booleanNode('generateId')->defaultTrue()->info('Automatically add an id field to entities')->end()
-                ->booleanNode('useInterface')->defaultFalse()->info('Generate interfaces and use Doctrine\'s Resolve Target Entity feature')->end()
-                ->booleanNode('checkIsGoodRelations')->defaultFalse()->info('Emit a warning if a property is not derived from GoodRelations')->end()
-                ->scalarNode('header')->defaultFalse()->info('A license or any text to use as header of generated files')->example('// (c) Kévin Dunglas <dunglas@gmail.com>')->end()
-                ->arrayNode('namespaces')
-                    ->addDefaultsIfNotSet()
-                    ->info('PHP namespaces')
-                    ->children()
-                        ->scalarNode('entity')->defaultValue('SchemaOrg\Entity')->info('The namespace of the generated entities')->example('Acme\Entity')->end()
-                        ->scalarNode('enum')->defaultValue('SchemaOrg\Enum')->info('The namespace of the generated enumerations')->example('Acme\Enum')->end()
-                        ->scalarNode('interface')->defaultValue('SchemaOrg\Model')->info('The namespace of the generated interfaces')->example('Acme\Model')->end()
-                    ->end()
-                ->end()
-                ->arrayNode('doctrine')
-                    ->addDefaultsIfNotSet()
-                    ->info('Doctrine')
-                    ->children()
-                        ->booleanNode('useCollection')->defaultTrue()->info('Use Doctrine\'s ArrayCollection instead of standard arrays')->end()
-                        ->scalarNode('resolveTargetEntityConfigPath')->defaultNull()->info('The Resolve Target Entity Listener config file pass')->end()
-                    ->end()
-                ->end()
-                ->scalarNode('author')->defaultFalse()->info('The value of the phpDoc\'s @author annotation')->example('Kévin Dunglas <dunglas@gmail.com>')->end()
-                ->enumNode('fieldVisibility')->values(['private', 'protected', 'public'])->defaultValue('private')->cannotBeEmpty()->info('Visibility of entities fields')->end()
-                ->arrayNode('types')
-                    ->info('Schema.org\'s types to use')
-                    ->useAttributeAsKey('id')
-                    ->prototype('array')
-                        ->children()
-                            ->scalarNode('vocabularyNamespace')->defaultValue(TypesGenerator::SCHEMA_ORG_NAMESPACE)->info('Namespace of the vocabulary the type belongs to.')->end()
-                            ->booleanNode('abstract')->defaultNull()->info('Is the class abstract? (null to guess)')->end()
-                            ->arrayNode('namespaces')
-                                ->addDefaultsIfNotSet()
-                                ->info('Type namespaces')
-                                ->children()
-                                    ->scalarNode('class')->defaultNull()->info('The namespace for the generated class (override any other defined namespace)')->end()
-                                    ->scalarNode('interface')->defaultNull()->info('The namespace for the generated interface (override any other defined namespace)')->end()
-                                ->end()
-                            ->end()
-                            ->arrayNode('doctrine')
-                                ->addDefaultsIfNotSet()
-                                ->children()
-                                    ->scalarNode('inheritanceMapping')->defaultNull()->info('The Doctrine inheritance mapping type (override the guessed one)')->end()
-                                ->end()
-                            ->end()
-                            ->scalarNode('parent')->defaultNull()->info('The parent class, set to false for a top level class')->end()
-                            ->arrayNode('properties')
-                                ->info('Properties of this type to use')
-                                ->useAttributeAsKey('id')
-                                ->prototype('array')
-                                    ->addDefaultsIfNotSet()
-                                    ->children()
-                                        ->scalarNode('range')->defaultNull()->info('The property range')->example('Offer')->end()
-                                        ->scalarNode('relationTableName')->defaultNull()->info('The relation table name')->example('organization_member')->end()
-                                        ->enumNode('cardinality')->defaultValue(CardinalitiesExtractor::CARDINALITY_UNKNOWN)->values([
-                                            CardinalitiesExtractor::CARDINALITY_0_1,
-                                            CardinalitiesExtractor::CARDINALITY_0_N,
-                                            CardinalitiesExtractor::CARDINALITY_1_1,
-                                            CardinalitiesExtractor::CARDINALITY_1_N,
-                                            CardinalitiesExtractor::CARDINALITY_N_0,
-                                            CardinalitiesExtractor::CARDINALITY_N_1,
-                                            CardinalitiesExtractor::CARDINALITY_N_N,
-                                            CardinalitiesExtractor::CARDINALITY_UNKNOWN,
-                                        ])->end()
-                                        ->arrayNode('groups')
-                                            ->info('Symfony Serialization Groups')
-                                            ->prototype('scalar')->end()
-                                        ->end()
-                                        ->scalarNode('nullable')->defaultTrue()->info('The property nullable')->end()
-                                        ->scalarNode('unique')->defaultFalse()->info('The property unique')->end()
-                                    ->end()
-                                ->end()
-                            ->end()
-                        ->end()
-                    ->end()
-                ->end()
-                ->arrayNode('annotationGenerators')
-                    ->info('Annotation generators to use')
-                    ->defaultValue([
-                        'ApiPlatform\SchemaGenerator\AnnotationGenerator\PhpDocAnnotationGenerator',
-                        'ApiPlatform\SchemaGenerator\AnnotationGenerator\ConstraintAnnotationGenerator',
-                        'ApiPlatform\SchemaGenerator\AnnotationGenerator\DoctrineOrmAnnotationGenerator',
-                    ])
-                    ->prototype('scalar')->end()
-                ->end()
+            ->arrayNode('rdfa')
+            ->info('RDFa files to use')
+            ->defaultValue([
+                self::SCHEMA_ORG_RDFA_URL,
+            ])
+            ->prototype('scalar')->end()
             ->end()
-            ;
+            ->arrayNode('relations')
+            ->info('OWL relation files to use')
+            ->defaultValue([
+                self::GOOD_RELATIONS_OWL_URL,
+            ])
+            ->prototype('scalar')->end()
+            ->end()
+            ->booleanNode('debug')->defaultFalse()->info('Debug mode')->end()
+            ->booleanNode('generateId')->defaultTrue()->info('Automatically add an id field to entities')->end()
+            ->booleanNode('useInterface')->defaultFalse()->info('Generate interfaces and use Doctrine\'s Resolve Target Entity feature')->end()
+            ->booleanNode('checkIsGoodRelations')->defaultFalse()->info('Emit a warning if a property is not derived from GoodRelations')->end()
+            ->scalarNode('header')->defaultFalse()->info('A license or any text to use as header of generated files')->example('// (c) Kévin Dunglas <dunglas@gmail.com>')->end()
+            ->arrayNode('namespaces')
+            ->addDefaultsIfNotSet()
+            ->info('PHP namespaces')
+            ->children()
+            ->scalarNode('entity')->defaultValue('SchemaOrg\Entity')->info('The namespace of the generated entities')->example('Acme\Entity')->end()
+            ->scalarNode('enum')->defaultValue('SchemaOrg\Enum')->info('The namespace of the generated enumerations')->example('Acme\Enum')->end()
+            ->scalarNode('interface')->defaultValue('SchemaOrg\Model')->info('The namespace of the generated interfaces')->example('Acme\Model')->end()
+            ->end()
+            ->end()
+            ->arrayNode('doctrine')
+            ->addDefaultsIfNotSet()
+            ->info('Doctrine')
+            ->children()
+            ->booleanNode('useCollection')->defaultTrue()->info('Use Doctrine\'s ArrayCollection instead of standard arrays')->end()
+            ->scalarNode('resolveTargetEntityConfigPath')->defaultNull()->info('The Resolve Target Entity Listener config file pass')->end()
+            ->end()
+            ->end()
+            ->scalarNode('author')->defaultFalse()->info('The value of the phpDoc\'s @author annotation')->example('Kévin Dunglas <dunglas@gmail.com>')->end()
+            ->enumNode('fieldVisibility')->values(['private', 'protected', 'public'])->defaultValue('private')->cannotBeEmpty()->info('Visibility of entities fields')->end()
+            ->arrayNode('types')
+            ->info('Schema.org\'s types to use')
+            ->useAttributeAsKey('id')
+            ->prototype('array')
+            ->children()
+            ->scalarNode('vocabularyNamespace')->defaultValue(TypesGenerator::SCHEMA_ORG_NAMESPACE)->info('Namespace of the vocabulary the type belongs to.')->end()
+            ->booleanNode('abstract')->defaultNull()->info('Is the class abstract? (null to guess)')->end()
+            ->arrayNode('namespaces')
+            ->addDefaultsIfNotSet()
+            ->info('Type namespaces')
+            ->children()
+            ->scalarNode('class')->defaultNull()->info('The namespace for the generated class (override any other defined namespace)')->end()
+            ->scalarNode('interface')->defaultNull()->info('The namespace for the generated interface (override any other defined namespace)')->end()
+            ->end()
+            ->end()
+            ->arrayNode('doctrine')
+            ->addDefaultsIfNotSet()
+            ->children()
+            ->scalarNode('inheritanceMapping')->defaultNull()->info('The Doctrine inheritance mapping type (override the guessed one)')->end()
+            ->end()
+            ->end()
+            ->scalarNode('parent')->defaultNull()->info('The parent class, set to false for a top level class')->end()
+            ->arrayNode('properties')
+            ->info('Properties of this type to use')
+            ->useAttributeAsKey('id')
+            ->prototype('array')
+            ->addDefaultsIfNotSet()
+            ->children()
+            ->scalarNode('range')->defaultNull()->info('The property range')->example('Offer')->end()
+            ->scalarNode('relationTableName')->defaultNull()->info('The relation table name')->example('organization_member')->end()
+            ->enumNode('cardinality')->defaultValue(CardinalitiesExtractor::CARDINALITY_UNKNOWN)->values([
+                CardinalitiesExtractor::CARDINALITY_0_1,
+                CardinalitiesExtractor::CARDINALITY_0_N,
+                CardinalitiesExtractor::CARDINALITY_1_1,
+                CardinalitiesExtractor::CARDINALITY_1_N,
+                CardinalitiesExtractor::CARDINALITY_N_0,
+                CardinalitiesExtractor::CARDINALITY_N_1,
+                CardinalitiesExtractor::CARDINALITY_N_N,
+                CardinalitiesExtractor::CARDINALITY_UNKNOWN,
+            ])->end()
+            ->arrayNode('groups')
+            ->info('Symfony Serialization Groups')
+            ->prototype('scalar')->end()
+            ->end()
+            ->scalarNode('nullable')->defaultTrue()->info('The property nullable')->end()
+            ->scalarNode('unique')->defaultFalse()->info('The property unique')->end()
+            ->end()
+            ->end()
+            ->end()
+            ->end()
+            ->end()
+            ->end()
+            ->arrayNode('annotationGenerators')
+            ->info('Annotation generators to use')
+            ->defaultValue([
+                'ApiPlatform\SchemaGenerator\AnnotationGenerator\PhpDocAnnotationGenerator',
+                'ApiPlatform\SchemaGenerator\AnnotationGenerator\ConstraintAnnotationGenerator',
+                'ApiPlatform\SchemaGenerator\AnnotationGenerator\DoctrineOrmAnnotationGenerator',
+            ])
+            ->prototype('scalar')->end()
+            ->end()
+            ->end()
+        ;
 
         return $treeBuilder;
     }
